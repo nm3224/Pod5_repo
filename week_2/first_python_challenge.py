babaf8a41a442dcf09e0e1fa3966a0ceae03dea5
Lillie Schachter <lillieschachter@gmail.com>
2022-02-07 19:55:07 -0500

    remove answers to first challenge

--- week_2/first_python_challenge.py
+++ week_2/first_python_challenge.py
@@ -1,24 +1,24 @@
 print('1.1: Create a variable called `meal`, and save a string describing what you had for lunch in it')
-meal = "I had shrimp fried rice and a shrimp roll for lunch"
+
 
 print('1.2: Print the meal variable')
-print(meal)
+
 
 print('1.3: Update the meal variable to be a string describing what you want for dinner. print it out again')
-meal = "I want fish tacos for dinner"
-print(meal)
+
+
 print('2: How old is Google?')
 # 2.1: Google was founded in 1993. The current year is 2022. Create a variable called google_age, and use subraction
 # to figure out how old Google is
 # ex: my_age = current_year - birth_year
-google_age = 2022 - 1993
-print(google_age)
 
-# 2.2: Using an f-string, print out a sentence about Google's age. Make sure to include your variable in the f-string!
-print(f'The age of Google is {google_age} years old.')
+
+# 2.2: Print out a sentence about Google's age. Make sure to include your variable in the f-string!
+
+
 
 # 2.3 How many _months_ old is Google? Create a new variable google_age_months, and use multiplication to figure it out,
-# then print a new f-string with the info.
+# then print the info.
 # (Assume 12 months for each year, you don't need to check which month they started)
 
 
