9a7524c9321aa5b57154acaf21ce1b482133dbae
jonfaustothen <jonfaustothen@gmail.com>
2022-02-08 17:23:06 -0500

    Adding temperature.py challenge and answer

---
+++ Jonathan/temperature.py
@@ -0,0 +1,33 @@
+print ('Welcome to the Temperature Calculator!')
+
+# print ('1. Creating variable for 100 Degrees F to C')
+fahrenheit_temp = 100
+celsius_100 = (fahrenheit_temp - 32) * (5/9)
+print (celsius_100)
+
+# print ('When F is 100 degrees, result is 37.77778 degrees C, a float.')
+
+# print ('2.Creating variable for 0 Degrees F to C')
+fahrenheit_temp = 0
+celsius_0 = (fahrenheit_temp - 32) * (5/9)
+print (celsius_0)
+
+# print ('3. Now using same variable above to convert new temperature.')
+fahrenheit_temp = 34.2
+celsius_0 = (fahrenheit_temp - 32) * (5/9)
+print (celsius_0)
+
+# print ('4. Creating new variable to convert from C to F.')
+celsius_temp = 5
+fahrenheit_0 = (celsius_temp * 9/5) + 32
+print(fahrenheit_0)
+
+# print ('5. What is hotter, a temperature of 30.2 Deg C, or a temperature of 85.1 Deg F?')
+# print ('-to find out, I will convert both temperatures to C')
+
+celsius_temp = 30.2
+fahrenheit_0 = (celsius_temp * 9/5) + 32
+print(fahrenheit_0)
+
+# print('-It turns out that 30.2 Deg C is equal to 85.36 Deg F, which is hotter than 85.1 Deg F.')
+
