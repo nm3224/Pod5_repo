9b17f170c80e7222f58afcf5338b995cb81ca2ce
Infinite Rahming <irahming83@gmail.com>
2022-02-15 10:31:01 -0500

    Adding Strings and Lists challenge

--- Infinite/strings_and_lists_challenge.py
+++ Infinite/strings_and_lists_challenge.py
@@ -3,9 +3,6 @@
 
 from tkinter.font import names
 
-
-employee_name = 'Ash Rahman'
-
 # You have decided the format of the email should be: Ash Rahman -> ash.rahman@ripplemedia.com
 # Let's write some code that converts a name into an email id that matches this format
 # 1.1 TODO: Let's save the lowercase version of the employee_name in a new variable 'lower_name' (use a string method to lower the name)
@@ -13,7 +10,7 @@ employee_name = 'Ash Rahman'
 # 1.3 TODO: We want to join the first name and last name with a '.' and save it in a variable called 'joined_names' (use a string method to join the list into a new string)
 # 1.4 TODO: We want to add '@ripplemedia.com' to the end of the string inside joined_names and save it in a variable 'email' (use an f-string to combine the username with the email domain)
 
-
+employee_name = 'Ash Rahman'
 lower_name = employee_name.lower()
 print(lower_name)
 
