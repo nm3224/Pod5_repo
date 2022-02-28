d98ceef73960aa405ecdf2797787c72a0def8bd8
Chung Kao <chungckao@gmail.com>
2022-03-01 01:50:11 +0800

    Correct oversight in playlist_challenge.py assignment

--- Chung/week_4/importing/playlist_functions.py
+++ Chung/week_4/importing/playlist_functions.py
@@ -47,5 +47,5 @@ This function should ALSO increase the 'plays' value for that song's dictionary 
 So, if 'Controversy' has 0 plays so far, it should now be increased to 1
 '''
 def play_track(playlist, track=1):
-	print(f"Now playing track {track}, {playlist[track-1]['title']}.")
+	print(f"Now playing track {track}, {playlist[track-1]['title']} by {playlist[track-1]['artist']}.")
 	playlist[track-1]['plays'] += 1
